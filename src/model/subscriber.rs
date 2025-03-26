1d0939ff991a61acaf819e2163ae53ad956d9bde
DanielAngger <angger.7d@gmail.com>
2025-03-26 23:15:14 +0700

    Create SubscriberRequest model struct.

---
+++ src/model/subscriber.rs
@@ -0,0 +1,8 @@
+Use rocket::serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket:: serde")]
+pub struct SubscriberRequest {
+    pub url: String, 
+    pub name: String,
+}
